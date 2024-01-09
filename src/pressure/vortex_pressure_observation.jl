c86388490e6050411f51e3ecaa688da7e34403b8
Hanieh Mousavi <hanieh.mousavi93@gmail.com>
2024-01-09 12:26:39 -0800

    added routines.jl

--- src/pressure/vortex_pressure_observation.jl
+++ src/pressure/vortex_pressure_observation.jl
@@ -56,20 +56,3 @@ function observations(x::AbstractVector,t,Δt,obs::VortexPressure,i::Int64)
 
     return dp
 end
-
-
-struct sens
-  x :: Vector{Float64}
-  y :: Vector{Float64}
-  Nsens :: Int64
-end
-
-"""Setting up sensors only for body::Plate for now"""
-function setup_sensors(pfb::PotentialFlowBody,Nsens)
-  @unpack points, U, Ω = pfb
-
-  xsens = range(points.x[1],points.x[end],length=Nsens)
-  ysens = range(points.y[1],points.y[end],length=Nsens)
-
-  return sens(vcat(xsens),vcat(ysens),Nsens)
-end
