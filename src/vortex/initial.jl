edc3af2410e3194d4e8daf946e2d153f814e53f7
Hanieh Mousavi <hanieh.mousavi93@gmail.com>
2024-01-10 15:56:48 -0800

    added initialize function

--- src/vortex/initial.jl
+++ src/vortex/initial.jl
@@ -1,38 +1,13 @@
-using Distributions, Statistics, LinearAlgebra
-
 import Base: size, length
 
 import Statistics: mean, var, std
 
-#=
-export initialize, initialize!
+export initialize!
 
-function initialize(N::Int, Distx0::MultivariateDistribution)
-    NS = length(Distx0)
-
-    # pre-allocate space
-    X0 = 
-    ENS = BasicEnsembleMatrix()
-    ENS = EnsembleState(N, zeros(NS))
-
-    ENS.S .= [rand(Distx0) for i = 1:N]
-    return ENS
-end
-#
-function initialize(N::Int, NS::Int)
-    # pre-allocate space
-    ENS = EnsembleState(N, zeros(NS))
-
-    Dist = MvNormal(zeros(NS), I)
-    ENS.S .= [rand(Dist) for i = 1:N]
-    return ENS
-end
-#
-function initialize!(X::BasicEnsembleMatrix{Nx, Ne, T})  where {Nx, Ne, T}
-    Dist = MvNormal(zeros(Nx), I)
+function initialize!(X::BasicEnsembleMatrix{Ne, T}; Σi=I)  where {Ne, T}
+    Dist = [MvNormal(X.X[:,j], Σi) for j in 1:Ne]
     for j in 1:Ne
-        X(j) .= initialize(X(j))
+        X(j) .= rand(Dist[j])
     end
     return X
 end
-=#
