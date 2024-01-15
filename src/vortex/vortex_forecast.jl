b848d78e3bf18b9932da7eba1649cab2372be154
Hanieh Mousavi <hanieh.mousavi93@gmail.com>
2024-01-15 10:52:21 -0800

    added intermediate_bodies

--- src/vortex/vortex_forecast.jl
+++ src/vortex/vortex_forecast.jl
@@ -42,7 +42,12 @@ function forecast(x::AbstractVector,t,Δt,fdata::VortexForecast{Nb,Ne},i::Int64)
 
     #solve the system for the existing vortices (vortexmode.jl)
     #which solve solve!(sol::ConstrainedIBPoissonSolution, vm::VortexModel{Nb,0,ConstrainedIBPoisson{Nb,TU,TF}})
-    intermediate_vm = VortexModel(vm.g,vortices=[vm.vortices...],bodies=bodies)
+    intermediate_bodies = deepcopy(vm.bodies)
+    for i=1:Nb
+        intermediate_bodies[i].edges = Int64[]
+    end
+    
+    intermediate_vm = VortexModel(vm.g,vortices=[vm.vortices...],bodies=intermediate_bodies)
     sol = ConstrainedIBPoissonSolution(intermediate_vm._ψ, intermediate_vm._f, zeros(Float64,Nb), zeros(Float64,Ne))
     time_advancement!(intermediate_vm,sol,Δt)
     vm.vortices = deepcopy(intermediate_vm.vortices)
