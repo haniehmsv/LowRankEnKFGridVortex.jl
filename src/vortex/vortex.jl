a8f701c45fbc36a13d637d2a40ad66b2fbfdf39d
Hanieh Mousavi <hanieh.mousavi93@gmail.com>
2024-02-21 09:37:48 -0800

    modified mapping between state vector and vortices for given vorticity flux at the LE

--- src/vortex/vortex.jl
+++ src/vortex/vortex.jl
@@ -1,22 +1,18 @@
 export vortices_to_states!, states_to_vortices!
 
 
-function vortices_to_states!(x::AbstractVector,vm::VortexModel{Nb,Ne},sol::ConstrainedIBPoissonSolution,Δt) where {Nb,Ne}
+function vortices_to_states!(x::AbstractVector,vm::VortexModel{Nb,Ne},xold::AbstractVector,Δt) where {Nb,Ne}
     @inbounds for (i, vortex) in enumerate(vm.vortices)
         x[3i-2:3i] .= (vortex.x, vortex.y, vortex.Γ)
     end
-    @inbounds for k=1:Ne
-        x[end-Ne+k] = sol.δΓ_vec[k]/Δt
-    end
+    x[end] = xold[end]
 end
 
 function states_to_vortices!(vm::VortexModel{Nb,Ne},x::AbstractVector,Δt) where {Nb,Ne}
     @inbounds for (i, vortex) in enumerate(vm.vortices)
         [vortex.x, vortex.y, vortex.Γ] .= x[3i-2:3i]
     end
-    @inbounds for k=1:Ne
-        vm.vortices.Γ[end-Ne+k] = x[end-Ne+k]*Δt
-    end
+    vm.vortices.Γ[end-1] = x[end]*Δt
 end
 
 function states_to_vortices!(vm::VortexModel{Nb,Ne},x::AbstractVector) where {Nb,Ne}
