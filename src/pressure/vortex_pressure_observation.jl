e062b44647c54133e7e62e6d11efdce0678c0aa0
Hanieh Mousavi <hanieh.mousavi93@gmail.com>
2024-03-03 13:30:07 -0800

    modified observation model

--- src/pressure/vortex_pressure_observation.jl
+++ src/pressure/vortex_pressure_observation.jl
@@ -9,79 +9,79 @@ struct Sensor{T}
   Nsens :: Int64
 end
 
-mutable struct VortexPressure{Ny,withfreestream,DST} <: AbstractObservationOperator{Ny,true}
+mutable struct VortexPressure{Ny,withfreestream,DST,TVG,TX,TVF,TF,TP} <: AbstractObservationOperator{Ny,true}
     sens::Sensor
     config::VortexForecast
-    intermediate_vm :: VortexModel
+    # intermediate_vm :: VortexModel
     Δs::DST
+
+    """for pressure calculations"""
+    v̄::TVG
+    Xs::TX
+    v̄s::TVF
+    dp::TF
+    p̄::TP
 end
 
 function VortexPressure(sens::Sensor,config::VortexForecast)
     @unpack vvm= config
     vm = vvm[1]
-    intermediate_bodies = deepcopy(vm.bodies)
-    Nb = length(vm.bodies)
-    for j=1:Nb
-        intermediate_bodies[j].edges = Int64[]
-    end
-    intermediate_vm = VortexModel(vm.g,vortices=[vm.vortices...],bodies=intermediate_bodies,U∞=vm.U∞)
     withfreestream = vm.U∞ == 0.0 ? false : true
     Nv = config.Nv
     Nx = 3*Nv
     Ny = length(sens.x)
     Δs = dlengthmid(vm.bodies[1].points)
-    return VortexPressure{Ny,withfreestream,typeof(Δs)}(sens,config,intermediate_vm,Δs)
+    v̄ = Edges(Primal,vm._ψ)
+    Xs = VectorData(collect(vm.bodies))
+    v̄s = VectorData(Xs)
+    dp = ScalarData(Xs)
+    p̄ = Nodes(Primal,vm._ψ)
+    return VortexPressure{Ny,withfreestream,typeof(Δs),typeof(v̄),typeof(Xs),typeof(v̄s),typeof(dp),typeof(p̄)}(sens,config,Δs,v̄,Xs,v̄s,dp,p̄)
 end
 
 
 function observations(x::AbstractVector,t,Δt,obs::VortexPressure,i::Int64)
-    @unpack sens, config, intermediate_vm, Δs = obs
+    @unpack sens, config, Δs = obs
     @unpack vvm = config
     @unpack bodies = vvm[i] #i-th ensemble member
     #for 1 body for now
     pfb = bodies[1]
     @unpack points = pfb
 
+    #Solution at the current time step
     states_to_vortices!(vvm[i],x) #i-th ensemble member
-    vvm[i].bodies[1].Γ = -sum(vvm[i].vortices.Γ)
-    #solution at the current time step n
+    vvm[i].bodies[1].Γ = -sum(vvm[i].vortices.Γ[1:end-2])
     vmn = deepcopy(vvm[i])
-    construct_intermediate_model!(intermediate_vm,vmn)
-    soln = solve(intermediate_vm)
-    γn = soln.f./Δs;
+    vmn.vortices.Γ[end-1] = x[end]*Δt
+    subtractcirculation!(vmn.bodies, [vmn.vortices.Γ[end-1]])
+    soln = solve(vmn)
+    vmn.vortices.Γ[end] = soln.δΓ_vec[1]
+    subtractcirculation!(vmn.bodies, soln.δΓ_vec)
+    soln = solve(vmn)
+    γn = soln.f./Δs
 
     #solution at the next time step n+1
-    vm1 = deepcopy(vvm[i])
-    # setting the value for v_LE
-    states_to_vortices!(vm1,x,Δt)
-    subtractcirculation!(vm1.bodies, [vm1.vortices.Γ[end-1]])
-    # solving for v_TE
+    vm1 = deepcopy(vmn)
+    advect_vortices!(vm1,soln,Δt)
+    vLEnew, vTEnew = createsheddedvortices(points,vm1.vortices[end-1:end])
+    pushvortices!(vm1,vLEnew,vTEnew)
+    vm1.vortices.Γ[end-1] = x[end]*Δt
+    subtractcirculation!(vmn.bodies, [vmn.vortices.Γ[end-1]])
     solnp1 = solve(vm1)
     vm1.vortices.Γ[end] = solnp1.δΓ_vec[1]
     subtractcirculation!(vm1.bodies, solnp1.δΓ_vec)
-    # advecting all vortices
-    Xv = getvortexpositions(vm1)
-    Ẋv = deepcopy(Xv)
-    vortexvelocities!(Ẋv, vm1, solnp1.ψ)
-    Xv .= Xv .+ Ẋv*Δt
-    setvortexpositions!(vm1, Xv)
     solnp1 = solve(vm1)
     γnp1 = solnp1.f./Δs
 
-    v̄ = Edges(Primal,soln.ψ)
-    Xs = VectorData(collect(vmn.bodies))
-    v̄s = VectorData(Xs)
-    velocity!(v̄,soln.ψ,vmn.ilsys)
-    surface_velocity!(v̄s,v̄,vmn.ilsys)
+    velocity!(obs.v̄,soln.ψ,vmn.ilsys)
+    surface_velocity!(obs.v̄s,obs.v̄,vmn.ilsys)
 
-    dp = ScalarData(Xs)
-    pressurejump!(dp,γn,γnp1,v̄s,Δt,vmn.ilsys)
-    p̄ = Nodes(Primal,soln.ψ)
-    pressure!(p̄,v̄,dp,vmn.ilsys)
-    p⁺, p⁻ = sided_pressures(p̄,dp,vmn.ilsys)
+    pressurejump!(obs.dp,γn,γnp1,obs.v̄s,Δt,vmn.ilsys)
+    pressure!(obs.p̄,obs.v̄,obs.dp,vmn.ilsys)
+    p⁺, p⁻ = sided_pressures(obs.p̄,obs.dp,vmn.ilsys)
 
-    dp_sens = surface_interpolation(dp,pfb,sens)
+    dp_sens = surface_interpolation(obs.dp,pfb,sens)
 
-    return dp_sens, p̄, p⁺, p⁻
+    return dp_sens, obs.p̄, p⁺, p⁻
 end
 
