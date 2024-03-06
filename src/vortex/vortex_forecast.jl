2498988241695353e5c55e949c812555e9d2207d
Hanieh Mousavi <hanieh.mousavi93@gmail.com>
2024-03-05 18:34:48 -0800

    plate::Polygon

--- src/vortex/vortex_forecast.jl
+++ src/vortex/vortex_forecast.jl
@@ -70,7 +70,7 @@ function advect_vortices!(vm::VortexModel{Nb,Ne},sol::ConstrainedIBPoissonSoluti
 end
 
 # vortices released at one-third of the way from the edge to the last released vortex from that edge
-function createsheddedvortices(plate::Plate,oldvortices)
+function createsheddedvortices(plate::Polygon,oldvortices)
 
     vLE = Vortex(2/3*plate.x[1]+1/3*FD.value(oldvortices[end-1].x),2/3*plate.y[1]+1/3*FD.value(oldvortices[end-1].y),0.0)
     vTE = Vortex(2/3*plate.x[end]+1/3*FD.value(oldvortices[end].x),2/3*plate.y[end]+1/3*FD.value(oldvortices[end].y),0.0)
