384e505111357f22f2e9d1c91601b7c4925ad010
Hanieh Mousavi <hanieh.mousavi93@gmail.com>
2024-03-07 15:39:37 -0800

    modify functions to deal with StructArray

--- src/vortex/vortex.jl
+++ src/vortex/vortex.jl
@@ -1,7 +1,7 @@
 export vortices_to_states!, states_to_vortices!
 
 
-function vortices_to_states!(x::AbstractVector,vm::VortexModel{Nb,Ne},xold::AbstractVector,Δt) where {Nb,Ne}
+function vortices_to_states!(x::AbstractVector,vm::VortexModel{Nb,Ne},xold::AbstractVector) where {Nb,Ne}
     @inbounds for (i, vortex) in enumerate(vm.vortices)
         x[3i-2:3i] .= (vortex.x, vortex.y, vortex.Γ)
     end
@@ -9,14 +9,14 @@ function vortices_to_states!(x::AbstractVector,vm::VortexModel{Nb,Ne},xold::Abst
 end
 
 function states_to_vortices!(vm::VortexModel{Nb,Ne},x::AbstractVector,Δt) where {Nb,Ne}
-    @inbounds for (i, vortex) in enumerate(vm.vortices)
-        [vortex.x, vortex.y, vortex.Γ] .= x[3i-2:3i]
-    end
+    vm.vortices.x .= x[1:3:end-1]
+    vm.vortices.y .= x[2:3:end-1]
+    vm.vortices.Γ .= x[3:3:end-1]
     vm.vortices.Γ[end-1] = x[end]*Δt
 end
 
 function states_to_vortices!(vm::VortexModel{Nb,Ne},x::AbstractVector) where {Nb,Ne}
-    @inbounds for (i, vortex) in enumerate(vm.vortices)
-        [vortex.x, vortex.y, vortex.Γ] .= x[3i-2:3i]
-    end
+    vm.vortices.x .= x[1:3:end-1]
+    vm.vortices.y .= x[2:3:end-1]
+    vm.vortices.Γ .= x[3:3:end-1]
 end
