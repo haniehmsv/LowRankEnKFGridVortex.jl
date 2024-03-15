356b10f9a3ed29e40efae6471d6237fdf44ecd4a
Hanieh Mousavi <hanieh.mousavi93@gmail.com>
2024-03-15 15:31:08 -0700

    added Ny to method dispatch

--- src/pressure/vortex_pressure_observation.jl
+++ src/pressure/vortex_pressure_observation.jl
@@ -41,7 +41,7 @@ end
 
 
 
-function observations(x::AbstractVector,t,Δt,obs::VortexPressure{true,Nb,Ne,UnsteadyRegularizedIBPoisson{Nb,Ne}},i::Int64) where {Nb,Ne}
+function observations(x::AbstractVector,t,Δt,obs::VortexPressure{Ny,true,Nb,Ne,UnsteadyRegularizedIBPoisson{Nb,Ne}},i::Int64) where {Ny,Nb,Ne}
     @unpack sens, config, Δs = obs
     @unpack vvm = config
     @unpack bodies = vvm[i] #i-th ensemble member
@@ -81,7 +81,7 @@ function observations(x::AbstractVector,t,Δt,obs::VortexPressure{true,Nb,Ne,Uns
     return obs.dp, dp2   #dp_sens
 end
 
-function observations(x::AbstractVector,t,Δt,obs::VortexPressure{true,Nb,Ne,ConstrainedIBPoisson{Nb}},i::Int64) where {Nb,Ne}
+function observations(x::AbstractVector,t,Δt,obs::VortexPressure{Ny,true,Nb,Ne,ConstrainedIBPoisson{Nb}},i::Int64) where {Ny,Nb,Ne}
     @unpack sens, config, Δs = obs
     @unpack vvm = config
     @unpack bodies = vvm[i] #i-th ensemble member
