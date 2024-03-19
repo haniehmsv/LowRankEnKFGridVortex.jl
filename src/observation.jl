81d1f8419263eb8e0eda751f386fb28d3053c49e
Hanieh Mousavi <hanieh.mousavi93@gmail.com>
2024-03-19 11:03:48 -0700

    removed Ny from EnsemleMatrix type

--- src/observation.jl
+++ src/observation.jl
@@ -62,7 +62,7 @@ Compute the observation function `h` for each of the states in `X` and place the
 The function `h` should take as inputs a Ny-dimensional vector of measurement points (`sens`), a vector of vortices,
 and the configuration data `config`.
 """
-function observations!(Y::EnsembleMatrix{Ny,Ne},X::EnsembleMatrix{Ne},t,Δt,obs::AbstractObservationOperator{Ny}) where {Ny,Ne}
+function observations!(Y::EnsembleMatrix{Ne},X::EnsembleMatrix{Ne},t,Δt,obs::AbstractObservationOperator{Ny}) where {Ny,Ne}
   for j in 1:Ne
       Y(j) .= observations(X(j),t,Δt,obs)
   end
